240f53e3918f7ba02dc0a31b4abe91c03b2179bf
nkritsky <nick.kritsky@gmail.com>
2022-01-01 14:10:38 +0300

    Create integer_divide.py

---
+++ integer_divide.py
@@ -0,0 +1,5 @@
+# Given two integers: x and y; return x ÷ y without using / (division) or * (multiplication) operators.
+# Note: Assume that both integers are positive.
+
+def integer_divide(x, y):
+  return x
