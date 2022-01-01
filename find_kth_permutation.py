3924250d0d2238d5d4aa43627ad977331bcbd602
nkritsky <nick.kritsky@gmail.com>
2022-01-01 13:55:30 +0300

    wors without recursion

--- find_kth_permutation.py
+++ find_kth_permutation.py
@@ -1,2 +1,21 @@
+def factorial(n):
+  if n==1:
+    return 1
+  else:
+    return n*factorial(n-1)
+
 def find_kth_permutation(v, k, result):
-  return result
+  v_len = len(v)
+  i = 0
+  perms = factorial(v_len)
+  print ("for the vector {} there will be {} permutations".format(v,perms))
+  while v_len > 0 :
+    result += str(v[((k-1)//(perms//v_len))])
+    print (v[((k-1)//(perms//v_len))])
+    del(v[((k-1)//(perms//v_len))])
+    k = (k-1) % (perms // v_len)
+    perms = perms // v_len
+    v_len -= 1
+  print(result)
+
+  return 
