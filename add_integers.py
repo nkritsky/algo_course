a6ec4a51a8da5e9fd230e6f74c13b933a20d6878
nkritsky <nick.kritsky@gmail.com>
2022-01-01 11:29:48 +0300

    working code

--- add_integers.py
+++ add_integers.py
@@ -1,12 +1,34 @@
 def add_integers(integer1, integer2):
-  result_head=None
-  result_tail=None
-  carry=0
-  if integer1==None:
-    return integer2
-  elif integer2==None:
-    return integer1
+  carry = 0
+  result = None
   while integer1 or integer2:
-    carry=carry+integer1.data+integer2.data
-    result_head.data=LinkedListNode(integer1.data+integer2.data)
-  return integer1
+    if integer1 == None:
+      while integer2:
+        sum=integer2.data + carry
+        result_cur.next=LinkedListNode(sum % 10)
+        result_cur=result_cur.next
+        integer2=integer2.next
+        carry = sum // 10
+    elif integer2 == None:
+      while integer1:
+        sum = integer1.data + carry
+        result_cur.next=LinkedListNode(sum % 10)
+        result_cur=result_cur.next
+        integer1=integer1.next
+        carry = sum // 10
+    else:
+      sum = integer1.data + integer2.data + carry
+      if result == None:
+        result=LinkedListNode(sum % 10)
+        result_cur=result
+      else:
+        result_cur.next=LinkedListNode(sum % 10)
+        result_cur=result_cur.next
+      carry = sum // 10
+      integer1 = integer1.next
+      integer2 = integer2.next
+  if carry > 0:
+    result_cur.next=LinkedListNode(carry)
+      
+
+  return result
